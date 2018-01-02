6a4b09a076f6767abcfdcb79efea5880f998f318
Zakary Kurzawski <zakarykurzawski@Zakarys-MacBook-Pro.local>
2018-01-02 13:14:46 -0600

    initial ccommit
    ''

---
+++ slide_menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  slide_menu
+//
+//  Created by Zakary Kurzawski on 1/2/18.
+//  Copyright © 2018 Zakary Kurzawski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
