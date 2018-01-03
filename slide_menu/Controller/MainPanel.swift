975dcbc9c92ec651b098d9db39ec439ed6044482
Zakary Kurzawski <zakarykurzawski@Zakarys-MacBook-Pro.local>
2018-01-02 22:59:38 -0600

    created animation and pan response for slide out menu

--- slide_menu/Controller/MainPanel.swift
+++ slide_menu/Controller/MainPanel.swift
@@ -22,8 +22,46 @@ class MainPanel: UIViewController {
         sideMenu.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         sideMenu.layer.shadowOpacity = 1
         sideMenu.layer.shadowOffset = CGSize(width: 8, height: 0)
+        
+        viewConstraint.constant = -180
+        
     
     }
 
+    @IBAction func panCompleted(_ sender: UIPanGestureRecognizer) {
+        
+        if sender.state == .began || sender.state == .changed {
+            
+            let translation = sender.translation(in: self.view).x
+            
+            if translation > 0 {
+                if viewConstraint.constant < 10 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.viewConstraint.constant += translation / 10
+                        self.view.layoutIfNeeded()
+                    })
+                }
+            } else {
+                if viewConstraint.constant > -180 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.viewConstraint.constant += translation / 10
+                        self.view.layoutIfNeeded()
+                    })
+                }
+            }
+            
+        } else if sender.state == .ended {
+            if viewConstraint.constant < -100 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.viewConstraint.constant = -180
+                    self.view.layoutIfNeeded()
+            })
+            } else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.viewConstraint.constant = 0
+                    self.view.layoutIfNeeded()
+                })
+                }
+            }
+        }
 }
-
